db5418c3b69c0f459116c27f2af52be33583d85b
Fatinm <fatinm@live.se>
2015-04-01 20:58:38 +0200

    Klar, redo för rättning

--- assignment_1/src/main/java/k3/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/k3/assignment_1/MainActivity.java
@@ -2,16 +2,46 @@ package k3.assignment_1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
 
 
 public class MainActivity extends ActionBarActivity {
+    Random random = new Random();
+    int i = 0;
+    private Button mButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Part 4 of the assignment - LogCat
+        Log.i("App 1 - Assignment 1","App started");
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+    }
+
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("App 1 - Assignment 1", "Button pressed");
+                String[] qQuotes = getResources().getStringArray(R.array.quotes_array);
+                TextView tv1 = (TextView) findViewById(R.id.textView);
+
+                i = random.nextInt(5);
+                tv1.setText(qQuotes[i]);
+            }
+        };
     }
 
 
@@ -35,5 +65,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("App 1 - Assignment 1:", "App has been paused");
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.i("App 1 - Assignment 1:", "App has been closed");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume(){
+        Log.i("App 1 - Assignment 1:", "App has resumed");
+        super.onResume();
     }
 }
